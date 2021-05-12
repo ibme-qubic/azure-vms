90d3f91c8e7967e60518d067754332079bca1e4d
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-05-12 16:57:56 +0100

    Various tweaks to master VM setup script

--- vm_setup.sh
+++ vm_setup.sh
@@ -3,26 +3,16 @@
 #### Update and install required software
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install xfce4 xrdp firefox pwgen gedit -y
+sudo apt-get install xfce4 xrdp firefox pwgen gedit build-essential unzip -y
 
 #### Enable remote desktop
 sudo systemctl enable xrdp
 echo xfce4-session >$HOME/.xsession
-echo "source $HOME/.profile" >$HOME/.xsessionrc
+echo ". $HOME/.profile" >$HOME/.xsessionrc
 # The following fixes crash on Windows client
 # see https://github.com/neutrinolabs/xrdp/issues/302
 sudo sed -i 's/allow_channels=true/allow_channels=false/g' /etc/xrdp/xrdp.ini
 sudo service xrdp restart
-
-# Add tutorial users. We only plan to use one but create a few so we
-# can if necessary put people on the same VM as another
-#for USER in ismrm1 ismrm2 ismrm3 ismrm4
-#do
-#    sudo adduser $USER
-#    sudo cp $HOME/.xsession $HOME/.xsessionrc $HOME/.profile /home/$USER/
-#    sudo chown $USER /home/$USER/.xsession* /home/$USER/.profile
-#    sudo chgrp $USER /home/$USER/.xsession* /home/$USER/.profile
-#done
 
 #### Mount data disk
 # WARNING: Azure does not guarantee which disk (sda, sdb, sdc, etc) will be the data disk so we need
@@ -36,13 +26,7 @@ sudo mkdir /data && sudo mount /dev/${DATADISK}1 /data
 UUID=`lsblk -f |grep ${DATADISK}1 |awk '{print $3}'`
 echo -e "UUID=$UUID /data\text4\tdefaults\t0\t2" > datadisk_fstab
 sudo sh -c 'cat datadisk_fstab >> /etc/fstab'
-rm datadisk_fstab
-
-#### Download tutorial data
-cd /data
-sudo wget https://fsl.fmrib.ox.ac.uk/fslcourse/downloads/asl.tar.gz
-sudo tar -xzf asl.tar.gz
-cd
+#rm datadisk_fstab
 
 #### Install FSL
 # We run the installer in quiet (non interactive) mode
@@ -56,13 +40,32 @@ sudo sed -i -e 's/(parent=parent, ready=ready)/(ready=ready, raiseErrors=True)/'
 # Set up environment only for azureuser - the -E switch doesn't appear to work
 python fslinstaller.py -e -q -d $FSLDIR
 
-#### Install quantiphyse
-For some reason there is a root-owned .conda dir here
-sudo rm -rf /home/azureuser/.conda
-$FSLDIR/fslpython/bin/conda init bash
-source /home/azureuser/.bashrc
-conda create -n qp python=3.7
-conda activate qp
-conda install pyside2
-pip install quantiphyse
+#### Add tutorial users. 
+# We only plan to use one but create a few so we
+# can if necessary put people on the same VM as another
+for USER in qp user1 user2 user3 user4
+do
+    sudo adduser $USER
+    echo xfce4-session | sudo tee -a /home/$USER/.xsession
+    echo ". /home/$USER/.profile" | sudo tee -a /home/$USER/.xsessionrc
+    sudo cp $HOME/.profile /home/$USER/
+    sudo mkdir -p /home/$USER/Desktop
+    sudo cp $HOME/*.desktop /home/$USER/Desktop/
+    sudo chmod a+x /home/$USER/Desktop/*.desktop
+    sudo chown $USER /home/$USER/.xsession* /home/$USER/.profile /home/$USER/Desktop /home/$USER/Desktop/*.desktop
+    sudo chgrp $USER /home/$USER/.xsession* /home/$USER/.profile /home/$USER/Desktop /home/$USER/Desktop/*.desktop
+    sudo ln -s /data /home/$USER/course_data
+    sudo ln -s /data /home/$USER/Desktop/course_data
+done
 
+#### Install quantiphyse in the qp user
+# Needs to be run interactively using su at present
+# because cannot get su and conda to work correctly in batch...
+#$FSLDIR/fslpython/bin/conda init bash
+#. /home/azureuser/.bashrc
+#conda create -n qp python=3.7
+#conda activate qp
+#conda install tensorflow tensorflow-probability
+#pip install quantiphyse
+#pip install quantiphyse-cvr quantiphyse-dsc quantiphyse-asl quantiphyse-cest quantiphyse-qbold tensorflow-probability quantiphyse-fsl oxasl==0.1.12
+#conda install pyside2 sphinx rst2pdf
